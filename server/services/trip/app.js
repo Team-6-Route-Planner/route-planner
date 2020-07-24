d701e1f4c9736ad2b9b586716045dee13a5487ac
Muhammad Jihad Robbani <jihad.robbani@yahoo.com>
2020-07-24 17:52:17 +0700

    refactor app.js to start.js
    - add start.js
    
    Todo:
    - trip test

--- server/services/trip/app.js
+++ server/services/trip/app.js
@@ -3,19 +3,12 @@ if(process.env.NODE_ENV !== 'production'){
 }
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 3001;
 const cors = require('cors');
 const mongo = require('./configs/mongo');
 
-mongo.connect(function(err) {
-    if(!err){
-        app.use(cors());
-        app.use(express.json());
-        app.use(express.urlencoded({ extended:false }));
-        app.use('/', require('./routes'));
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended:false }));
+app.use('/', require('./routes'));
 
-        app.listen(PORT, function() {
-            console.log(`Server trip running on port ${PORT}`);
-        });
-    }
-});
+module.exports = app;
