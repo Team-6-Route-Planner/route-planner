03837fd8740963da84c75c26de253605583f81d0
Muhammad Jihad Robbani <jihad.robbani@yahoo.com>
2020-07-24 12:19:15 +0700

    change port
    3000 to 3001

--- server/services/trip/app.js
+++ server/services/trip/app.js
@@ -3,7 +3,7 @@ if(process.env.NODE_ENV !== 'production'){
 }
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 const cors = require('cors');
 const mongo = require('./configs/mongo');
 
