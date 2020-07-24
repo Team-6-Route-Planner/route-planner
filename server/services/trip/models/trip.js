450a2bb0f986929bb9007b6607193b5bfe1fd26e
Muhammad Jihad Robbani <jihad.robbani@yahoo.com>
2020-07-24 17:57:10 +0700

    refactor app.js to start.js
    - add start.js

--- server/services/trip/models/trip.js
+++ server/services/trip/models/trip.js
@@ -1,5 +1,5 @@
-const { getDatabase } = require('../configs/mongo');
-const Trip = getDatabase().collection(process.env.COLLECTION_NAME);
+const db = require('../configs/mongo');
+const Trip = db.collection(process.env.COLLECTION_NAME);
 const { ObjectId } = require('mongodb');
 
 class TripModel {
