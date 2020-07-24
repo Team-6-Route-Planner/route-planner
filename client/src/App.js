3ca80875bd948b975c4c736abcfd5a806a5637a3
floatinfeel <dandythamrin@gmail.com>
2020-07-24 08:53:33 +0700

    client admin

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,4 @@
 import React from "react";
-/*import {
-  GoogleMap,
-  useLoadScript,
-  Marker,
-  InfoWindow,
-} from "@react-google-maps/api";
-import usePlacesAutocomplete, {
-  getGeocode,
-  getLatLng,
-} from "use-places-autocomplete";
-import {
-  Combobox,
-  ComboboxInput,
-  ComboboxPopover,
-  ComboboxList,
-  ComboboxOption,
-} from "@reach/combobox";*/
 
 import {
   BrowserRouter as Router, Switch, Route, Link
@@ -25,11 +8,11 @@ import Track from './pages/Track.js'
 
 export default function App() {
   return(
-    <div className="App">
+    <div className="app">
       <Router>
         <div className="Header container-fluid">
           <header className="d-flex justify-content-around">
-            <Link to="/">Home</Link>
+            <Link to="/">Home</Link> &nbsp;|&nbsp;
             <Link to="/track">Track Courier</Link>
           </header>
         </div>
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -3,6 +3,8 @@ import React from "react"
 export default function Home(){
 
 	return(
+	  <>
 		<h3>This is Home</h3>
+	  </>
 	)
 }
