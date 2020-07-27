71c5326616dea946b32e3a665af19f4e0df9deef
Muhammad Jihad Robbani <jihad.robbani@yahoo.com>
2020-07-27 11:14:38 +0700

    add get one trip

--- server/services/routes/tripRoutes.js
+++ server/services/routes/tripRoutes.js
@@ -3,9 +3,9 @@ const Controller = require("../controllers/tripController");
 
 router.post("/", Controller.add);
 router.get("/", Controller.list);
-// router.get("/:userId/all");
 router.get("/:userId/current", Controller.listOneCurrent);
 router.get("/:userId/history", Controller.showHistory);
+router.get("/:tripId", Controller.getTripById);
 router.put("/:id", Controller.edit);
 
 module.exports = router;
